eaf3562e54debd75197bb44ca268d758c08416e5
Briant Oliveira <briantmoliveira@gmail.com>
2017-04-24 21:16:20 -0400

    sending work

---
+++ BasicCards.js
@@ -0,0 +1,7 @@
+// Contructor funtion for the 'basic card'
+function BasicCard(front, back) {
+    this.front = front;
+    this.back = back; 
+};
+
+module.exports = Basiccard;
---
+++ Clozecard.js
@@ -0,0 +1,15 @@
+// Cloze constructor 
+function Clozecard(text, Cloze) {
+    this.text = text.split(cloze); 
+    this.cloze=cloze
+};
+
+function ClozecardPrototype() {
+    this.clozeRemoved = function(){
+        return $(this.text[0])} ... $(this.text[1])};
+    };
+};
+
+Clozecard.prototype = new ClozecardPrototype();
+
+module.exports =  Clozecard;
